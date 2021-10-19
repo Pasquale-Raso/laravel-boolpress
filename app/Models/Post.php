b1ea032df9a71d217da8c11dc3fcbf5e91022974
Pasquale-Raso <padqualeraso@gmail.com>
2021-10-19 22:25:29 +0200

    applico carbon alle date

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Models;
-
+//! creazione campo per le date con carbon
+use Carbon\Carbon;
+//! --------------------------------------
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //! creazione campo per le date con carbon   
+    public function getFormattedDate($column, $format = 'd-m-Y H:m')
+    {
+        return  Carbon::create($this->$column)->format($format);
+    }    
+    //! --------------------------------------
 }
